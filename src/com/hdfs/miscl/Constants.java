ea74b8ff12be5946789c3646f814b83b2013e8c3
shwetav1311 <cool.shwetav@gmail.com>
2016-08-21 23:17:47 +0530

    changes for list

--- src/com/hdfs/miscl/Constants.java
+++ src/com/hdfs/miscl/Constants.java
@@ -4,7 +4,7 @@ public final class Constants {
 
 
 	public static final String  NAME_NODE = "NameNode";  //added by shweta
-	public static final String  NAME_NODE_IP = "10.0.0.1";  //added by shweta
+	public static final String  NAME_NODE_IP = "127.0.0.1";  //added by shweta
 	public static final String DATA_NODE_ID = "DataNode";//constant by sheshadri
 	
 	
@@ -24,6 +24,6 @@ public final class Constants {
 	public static final long BLOCK_REPORT_FREQ = 5000;
 	public static final long HEART_BEAT_FREQ = 5000;
 
-	public static final String CONNECTIVITY = "eth0"; //eth0 if you use a cable
-//	public static final String CONNECTIVITY = "wlan0"; //eth0 if you use a cable
+//	public static final String CONNECTIVITY = "eth0"; //eth0 if you use a cable
+	public static final String CONNECTIVITY = "wlan0"; //eth0 if you use a cable
 }
